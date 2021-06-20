2733ac5eab122e4503ab0ac42fcc1cc03f81ca1d
Roman Klochkov <72462941+Korinsky@users.noreply.github.com>
2021-06-20 17:23:29 +0300

    Одна фикстура на три теста

--- fixture/group.py
+++ fixture/group.py
@@ -6,7 +6,7 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, group):
+    def create(self, group):
         wd = self.app.wd
         self.open_group_page()
         wd.find_element_by_xpath("//div[@id='content']/form/input[4]").click()
@@ -21,6 +21,15 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_pages()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_group_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        #submit deletion
+        wd.find_element_by_name("delete").click()
         self.return_to_groups_pages()
 
     def open_group_page(self):
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,19 +1,10 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.group import Group
-from fixture.application import Application
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.create_(Group(name="qwe", header="qwe", footer="qwe"))
+    app.group.create(Group(name="qwe", header="qwe", footer="qwe"))
     app.session.logout()
 
 
