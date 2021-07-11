b556fc986df4ce562c76b0f96a9e9764da3d5129
Roman Klochkov <72462941+Korinsky@users.noreply.github.com>
2021-07-11 18:33:49 +0300

    исправленное 8 задание

--- conftest.py
+++ conftest.py
@@ -16,11 +16,11 @@ def app(request):
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
+
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
-
     request.addfinalizer(fin)
     return fixture
--- fixture/session.py
+++ fixture/session.py
@@ -37,4 +37,4 @@ class SessionHelper:
 
     def is_logged_in(self):
         wd = self.app.wd
-        return len(wd.find_elements_by_link_text("")) > 0
+        return len(wd.find_elements_by_link_text("Logout")) > 0
