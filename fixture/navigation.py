b646c8f21ec58a28b1bc137abf5fdf91bc2c3e6e
Roman Klochkov <72462941+Korinsky@users.noreply.github.com>
2021-07-11 20:24:09 +0300

    Задание 10, оптимизация переходов

--- fixture/navigation.py
+++ fixture/navigation.py
@@ -4,12 +4,15 @@ class NavigationHelper:
 
     def open_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_name("searchstring")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def open_add_contact_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not (wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("firstname")) > 0):
+            wd.find_element_by_link_text("add new").click()
 
     def open_group_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
