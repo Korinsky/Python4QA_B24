50538efe3698217c4a1c4b3d0e35915172331ca8
Roman Klochkov <72462941+Korinsky@users.noreply.github.com>
2021-07-29 23:45:42 +0300

    interm - ORM base

--- fixture/group.py
+++ fixture/group.py
@@ -74,3 +74,15 @@ class GroupHelper:
                 id = element.find_element_by_name("selected[]").get_attribute("value")
                 self.group_cache.append(Group(name=text, id=id))
         return list(self.group_cache)
+
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.app.navigation.open_group_page()
+        self.select_group_by_index_id(id)
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_pages()
+        self.group_cache = None
+
+    def select_group_by_index_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
