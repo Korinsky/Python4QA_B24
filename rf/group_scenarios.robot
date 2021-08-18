0c5d5f812a6cb858a3bf59e45745a7fce206fd7e
Roman Klochkov <72462941+Korinsky@users.noreply.github.com>
2021-08-18 16:05:14 +0300

    interm - RF fail

---
+++ rf/group_scenarios.robot
@@ -0,0 +1,9 @@
+*** Setting ***
+Library   rf.AddressBook
+Suite Setup   Init Fixtures
+Suite Teardown   Destroy Fixture
+
+
+*** Test Case ***
+Add new group
+    Create Group   name1  header1  footer1
--- test/test_group_add.py
+++ test/test_group_add.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-
+import pytest
 
 def test_add_group(app, db, json_groups, check_ui):
     group = json_groups
-    old_groups = db.get_groups_list()
-    app.group.create(group)
-    new_groups = db.get_groups_list()
-    old_groups.append(group)
-    assert old_groups == new_groups
-    if check_ui:
-        assert sorted(old_groups, key=Group.id_or_max) == sorted(app.group.get_groups_list(), key=Group.id_or_max)
+    with pytest.allure.step("Given a group list"):
+        old_groups = db.get_groups_list()
+    with pytest.allure.step("When I add the group %s to the list" % group):
+        app.group.create(group)
+    with pytest.allure.step("Then the new group list is equal to the old list with the added group"):
+        new_groups = db.get_groups_list()
+        old_groups.append(group)
+        assert old_groups == new_groups
+        if check_ui:
+            assert sorted(old_groups, key=Group.id_or_max) == sorted(app.group.get_groups_list(), key=Group.id_or_max)
